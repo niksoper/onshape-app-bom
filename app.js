1b400eae7d4cdb39322c63244b65ef262cd4b717
Nicholas Soper <nick@rocketmakers.com>
2020-04-24 15:01:53 +0100

    Dev server logging

--- app.js
+++ app.js
@@ -132,6 +132,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 
 if (app.get('env') === 'development') {
+  console.log('USING DEV ERROR HANDLER...')
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
